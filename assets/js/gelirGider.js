b1f4bbfd2315dd159ce2dc9613b26f26047e63b6
Berat Dimen <beratefedimen@hotmail.com>
2024-06-28 17:00:55 +0300

    örnek tablo listesi eklendi

--- assets/js/gelirGider.js
+++ assets/js/gelirGider.js
@@ -9,6 +9,9 @@ let id = 0;
 if (localStorage.moneys) {
   moneys = JSON.parse(localStorage.moneys);
   renderMoneys();
+  addData.disabled = true;
+} else {
+  addData.disabled = false;
 }
 
 if (localStorage.id) {
@@ -20,6 +23,35 @@ function generateId() {
   localStorage.id = id;
   return id;
 }
+
+addData.addEventListener("click", function () {
+  moneys = [
+    ...moneys,
+    { id: 1, name: "Gelir Kaynağı 1", type: "gelir", money: 3481 },
+    { id: 2, name: "Gelir Kaynağı 2", type: "gelir", money: 8420 },
+    { id: 3, name: "Gelir Kaynağı 3", type: "gelir", money: 1013 },
+    { id: 4, name: "Gelir Kaynağı 4", type: "gelir", money: 9057 },
+    { id: 5, name: "Gelir Kaynağı 5", type: "gelir", money: 4976 },
+    { id: 6, name: "Gelir Kaynağı 6", type: "gelir", money: 2134 },
+    { id: 7, name: "Gelir Kaynağı 7", type: "gelir", money: 6574 },
+    { id: 8, name: "Gelir Kaynağı 8", type: "gelir", money: 7812 },
+    { id: 9, name: "Gelir Kaynağı 9", type: "gelir", money: 3325 },
+    { id: 10, name: "Gelir Kaynağı 10", type: "gelir", money: 9187 },
+    { id: 11, name: "Gider Kaynağı 1", type: "gider", money: 1249 },
+    { id: 12, name: "Gider Kaynağı 2", type: "gider", money: 3210 },
+    { id: 13, name: "Gider Kaynağı 3", type: "gider", money: 1987 },
+    { id: 14, name: "Gider Kaynağı 4", type: "gider", money: 2976 },
+    { id: 15, name: "Gider Kaynağı 5", type: "gider", money: 1876 },
+    { id: 16, name: "Gider Kaynağı 6", type: "gider", money: 3450 },
+    { id: 17, name: "Gider Kaynağı 7", type: "gider", money: 2100 },
+    { id: 18, name: "Gider Kaynağı 8", type: "gider", money: 1750 },
+    { id: 19, name: "Gider Kaynağı 9", type: "gider", money: 4890 },
+    { id: 20, name: "Gider Kaynağı 10", type: "gider", money: 3021 },
+  ];
+  save();
+  renderMoneys();
+  addData.disabled = true;
+});
 
 addMoneyBtn.addEventListener("click", () => {
   modal.classList.remove("editModal");
@@ -34,7 +66,7 @@ function handleMovieForm() {
 
   if (formObj.id !== "") {
     let money = moneys.find((x) => x.id === Number(formObj.id));
-    money.selectValue = formObj.selectValue;
+    money.type = formObj.type;
     money.money = formObj.money;
     money.name = formObj.name;
   } else {
@@ -112,7 +144,7 @@ function handleEditBtn(e) {
 
   document.querySelector('input[name = "id"]').value = money.id;
   document.querySelector('input[name = "name"]').value = money.name;
-  document.querySelector('select[name = "selectValue"]').value = money.selectValue;
+  document.querySelector('select[name = "type"]').value = money.type;
   document.querySelector('input[name = "money"]').value = money.money;
   modal.showModal();
 }
@@ -124,11 +156,11 @@ function renderMoneys() {
   let sumExpense = 0;
   let sumIncome = 0;
   moneys.forEach((money) => {
-    if (money.selectValue === "gelir") {
+    if (money.type === "gelir") {
       income.innerHTML += createMoneyHtml(money);
       sumIncome += Number(money.money);
 
-    } else if (money.selectValue=== "gider") {
+    } else if (money.type=== "gider") {
       expense.innerHTML += createMoneyHtml(money);
       sumExpense += Number(money.money);
     }
